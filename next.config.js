8996da67d3e631b28730e31e3f8de1d8c54f44c8
zjpiazza <zjpiazza@gmail.com>
2025-01-28 16:25:02 -0600

    Overlayed supabase starter template

--- next.config.js
+++ next.config.js
@@ -1,39 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-    };
-    config.module.exprContextCritical = false;
-    return config;
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Resource-Policy',
-            value: 'cross-origin',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' blob: data:; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: https://unpkg.com; style-src 'self' 'unsafe-inline'; worker-src 'self' blob:; child-src blob:; connect-src 'self' https://unpkg.com blob:;"
-          }
-        ],
-      },
-    ];
-  },
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
